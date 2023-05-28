253f17b4e6e2ace1fb898c3a0270ea1144f04d49
meghnathbromon <meghnathbromon01@gmail.com>
2023-05-28 21:08:41 +0600

    orders page review items and implement delete button style

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,9 +7,11 @@ const Cart = ({cart}) => {
     // console.log(cart);
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for(const product of cart){
        totalPrice = totalPrice + product.price;
        totalShipping = totalShipping + product.shipping;
+       quantity = quantity + product.quantity;
     }
 
     const tax = totalPrice*7/100;
@@ -17,7 +19,7 @@ const Cart = ({cart}) => {
     return (
         <div className='cart'>
             <h2>Order Summary</h2>
-            <p>Selected items: {cart.length}</p>
+            <p>Selected items: {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping Charge: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
